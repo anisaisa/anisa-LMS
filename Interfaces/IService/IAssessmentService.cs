9d0369a37dae1265888a808d5fb7f5800f5d6b66
Anisa Isa <ai30776@seeu.edu.mk>
2026-06-05 23:56:36 +0200

    Implement assessments and scoring

---
+++ Interfaces/IService/IAssessmentService.cs
@@ -0,0 +1,13 @@
+﻿using anisa_lms.DTOs;
+
+namespace anisa_lms.Interfaces.IService
+{
+    public interface IAssessmentService
+    {
+        public Task<List<AssessmentDto>> GetUpcomingAssessments(int cId);
+        public Task<List<AssessmentScoreDto>> GetResults(int aId, bool passed);
+        public Task CreateAssessment(CreateAssessmentDto create,string instructorId, bool isAdmin);
+        public Task<bool?> UpdateAssessment(int aId, UpdateAssessmentDto update,string instructorId,bool isAdmin);
+        public Task<bool?> DeleteAssessment(int aId,string instructorId,bool isAdmin);
+    }
+}
